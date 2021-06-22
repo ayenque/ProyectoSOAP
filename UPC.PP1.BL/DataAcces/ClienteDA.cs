3760e180d3974b828552ac77896f27f39315c130
Angelo Yenque T <u201910967@upc.edu.pe>
2021-06-22 01:36:43 -0500

    Add project files.

---
+++ UPC.PP1.BL/DataAcces/ClienteDA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPC.PP1.BL.DataAcces
+{
+    class ClienteDA
+    {
+        private readonly BDPedidosDataContext dc;
+        public ClienteDA()
+        {
+            dc = new BDPedidosDataContext();
+
+        }
+
+        public int Registrar(Cliente objCliente)
+        {
+            try
+            {
+                dc.Clientes.InsertOnSubmit(objCliente);
+                return objCliente.nu_dni;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
